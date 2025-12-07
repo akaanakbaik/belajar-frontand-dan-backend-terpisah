73b4b63ee282d78f1d698acfd49b413fd45088be
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2025-12-07 10:04:41 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// Serve file statis dari folder public
 app.use(express.static(path.join(__dirname, '../public')));
-
-// Route fallback ke index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-// Penting untuk Vercel: Export app
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 module.exports = app;
